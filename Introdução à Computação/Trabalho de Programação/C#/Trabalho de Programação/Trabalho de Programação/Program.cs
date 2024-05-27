b2ec94094b9d6ac16d89fd5e4a2dca98b630d914
Gabrielle <gabriellemaia10@hotmail.com.br>
2024-05-27 12:09:16 -0300

    começando trabalho de ic

---
+++ Introdução à Computação/Trabalho de Programação/C#/Trabalho de Programação/Trabalho de Programação/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_de_Programação
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcaoMenu;
+
+            Console.WriteLine("Seja bem vindo, digite qual lugar quer acessar \n1 - Vendedor \n2-Cliente");
+            opcaoMenu = int.Parse(Console.ReadLine());  
+
+            while (opcaoMenu != 3) {
+               
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
