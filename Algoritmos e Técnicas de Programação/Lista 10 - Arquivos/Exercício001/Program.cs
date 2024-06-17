78a6e899c39ae715d7e68f0a89633a358b99b849
Gabrielle <gabriellemaia10@hotmail.com.br>
2024-06-17 12:14:44 -0300

    ex 01 pronto, começando o 2

---
+++ Algoritmos e Técnicas de Programação/Lista 10 - Arquivos/Exercício001/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+namespace Exercício001
+{
+    
+    internal class Program
+    {
+        static void ImprimeNomeCientifico(string[] nomesCientificos)
+        {
+            for (int i = 0; i < nomesCientificos.Length; i++)
+            {
+                Console.WriteLine($"Nome Cientifico do animal {i + 1}: {nomesCientificos[i]}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            string linha;
+            string[] temp = new string[5];
+            string[] nomesCientificos = new string[9];
+            int count = 0;
+
+            try 
+            {
+                StreamReader arquivo = new StreamReader("arquivo-01.txt", Encoding.UTF8);
+
+                linha = arquivo.ReadLine();
+                                        
+                while (linha != null)
+                {
+                    temp = linha.Split(';');
+                    nomesCientificos[count] = temp[1];
+
+                    for (int i = 0;i < temp.Length; i++)
+                    {
+                        temp[i] = "";
+                    }
+                    count++;
+                    linha = arquivo.ReadLine();
+
+                }
+                arquivo.Close();
+
+                ImprimeNomeCientifico(nomesCientificos);
+
+            }
+            catch(Exception e) 
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Algoritmos e Técnicas de Programação/Lista 10 - Arquivos/Exercício002/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace Exercício002
+{
+    internal class Program
+    {
+        static void ImprimeInfoPesos(string[] pesos)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            string linha;
+            string[] temp = new string[5];
+            string[] pesos = new string[9];
+
+            try
+            {
+                StreamReader();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
