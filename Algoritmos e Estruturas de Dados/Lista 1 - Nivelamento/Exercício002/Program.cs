5f40a67978980452972f8cb61c624c77bf5cc252
Gabrielle Maia <gabriellemaia10@hotmail.com.br>
2024-08-07 12:14:46 -0300

    exercício 1,2 e 3 incompletos

---
+++ Algoritmos e Estruturas de Dados/Lista 1 - Nivelamento/Exercício002/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, temp = 1;
+
+            Console.WriteLine("Digite um numero inteiro N, para exibir o triângulo de Floyd");
+            num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(temp + " ");
+                    temp++;
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
