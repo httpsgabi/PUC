7e0805eb48a461aafeb8fe774dd960a1ea89ca31
Gabrielle Maia <gabriellemaia10@hotmail.com.br>
2024-11-27 12:09:58 -0300

    .

---
+++ Algoritmos e Estruturas de Dados/Lista 7 - Pesquisa Binária e Árvore Binária de Pesquisa/Exercício002/ArvoreBinaria.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício002
+{
+    internal class ArvoreBinaria
+    {
+        private No raiz;
+
+        public ArvoreBinaria()
+        {
+            raiz = null;
+        }
+
+        public void Inserir(int x) {
+
+        }
+        public bool Pesquisar(int x) { 
+        
+        }
+        public void CaminharCentral() {
+        
+        }
+        public void CaminharPre() { 
+        
+        }
+        public void CaminharPos() {
+        
+        }
+        public void Remover(int x) { 
+        
+        }
+        public int GetMaior()
+        {
+
+        }; //método adicional
+        public int GetMaior()
+        {
+
+        }; //método adicional
+    }
+}
---
+++ Algoritmos e Estruturas de Dados/Lista 7 - Pesquisa Binária e Árvore Binária de Pesquisa/Exercício002/No.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício002
+{
+    internal class No
+    {
+        private int elemento;
+        private No esq;
+        private No dir;
+
+        public No(int elemento)
+        {
+            this.elemento = elemento;
+            esq = null;
+            dir = null;
+        }
+
+        public No(int elemento, No esq, No dir)
+        {
+            this.elemento = elemento;
+            this.esq = esq;
+            this.dir = dir;
+        }
+
+        public int Elemento
+        {
+            get { return elemento; }
+            set { elemento = value; }
+        }
+
+        public No Esq
+        {
+            get { return esq; } 
+            set { esq = value; }
+        }
+
+        public No Dir
+        {
+            get { return dir; }
+            set { dir = value; }
+        }
+    }
+}
